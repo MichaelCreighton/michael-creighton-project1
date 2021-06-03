d2d43b3120dd65534a1525ae922a4b3895add5d8
Michael <mikl@mymts.net>
2021-06-03 06:57:18 -0500

    finished hammy for assignment 2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// LOL! I would never do this on a page.
+// LOL! I don't think i'd ever do this on a page...
 // But if you just need to know if we can target elements
 // with JS, I thought you should at least be entertained.
 
@@ -12,11 +12,4 @@ navDisplay = document.querySelector('ul');
 // add an event listener to watch for a click on the icon
 hammy.addEventListener('click', function() {
   navDisplay.classList.toggle('no-show');
-  hammy.style.font = "font-size: 0.5rem;"
-  console.log(hammy.style);
-  if (navDisplay.classList === 'no-show') {
-    hammy.style.font = "font-size: 0.5rem;"
-    console.log(hammy.style);
-    
-  }
 });
